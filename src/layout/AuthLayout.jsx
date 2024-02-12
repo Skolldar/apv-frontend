3e336b37525a0360a54cc35e55990d9cbe3e5bad
Carla Pena <exoreiba@gmail.com>
2024-02-12 10:37:14 -0300

    mejora de mt y min-h-screen en login

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayout = () => {
     <>
     <div className="login min-h-screen">
 
-    <main className="container mx-auto md:grid md:grid-cols-2  mt-12 gap-20 p-5">
+    <main className="container mx-auto md:grid md:grid-cols-2   gap-20 p-5">
       <Outlet />
     </main>
     </div>
