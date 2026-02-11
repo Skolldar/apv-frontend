aca13a81cdf4dfc826d343306ee5359a95ef29d9
Carla <exoreiba@email.com>
2026-02-11 11:21:09 +0100

    Set default email and password values in Login component

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -9,8 +9,8 @@ import clienteAxios from '../config/axios';
 
 const Login = () => {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('test@test.com')
+    const [password, setPassword] = useState('Test@123')
     const [alerta, setAlerta] = useState({})
     const [showPassword, setShowPassword] = useState(false)
 
